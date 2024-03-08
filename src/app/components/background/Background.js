9fa19c251e3ccecd264ce9a5d1c842e8991cbae7
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-03-07 20:47:09 -0500

    JE botones

---
+++ src/app/components/background/Background.js
@@ -0,0 +1,11 @@
+
+const Background = () => {
+    return ( 
+        <div className="w-full h-screen relative flex">
+            <div className="w-full h-full bg-gray-200 " ></div>
+            <div className="absolute w-full h-[350px] bottom-0 shadow-t- bg-gray-100 " ></div>
+        </div>
+    );
+}
+ 
+export default Background;
---
+++ src/app/components/btns/BtnViewCar.js
@@ -0,0 +1,13 @@
+'use client'
+import { useState } from "react";
+
+const BtnViewCar = () => {
+
+    return (
+        <div className=" w-32 h-10 flex justify-center items-center rounded-full text-base cursor-pointer border-[2px] border-slate-700  ">
+            View Car
+        </div>
+    );
+}
+ 
+export default BtnViewCar;
