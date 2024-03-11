7a3ab713d86683c08c4033b113eb78e0adb7ac67
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-03-10 22:32:27 -0500

    JE y JS header

--- src/components/btns/BtnViewCar.js
+++ src/components/btns/BtnViewCar.js
@@ -4,7 +4,7 @@ import { useState } from 'react'
 const BtnViewCar = () => {
    return (
       <div className=" w-32 h-10 flex justify-center items-center rounded-full text-base cursor-pointer border-2 border-zinc-800 bg-zinc-800 text-zinc-200  relative  hover:bg-transparent hover:text-zinc-800 transition-all duration-500">
-         <div className="w-32 h-10 rounded-full absolute bg-zinc-900 opacity-10"></div>
+         <div className="w-full h-full rounded-full absolute bg-zinc-900 opacity-10"></div>
          ver vehículo
       </div>
    )
--- src/components/layout/HeaderMain.js
+++ src/components/layout/HeaderMain.js
@@ -1,12 +1,26 @@
+import { MdOutlineEmail } from "react-icons/md";
+import { BsTicketPerforated } from "react-icons/bs";
+
+
 const HeaderMain = () => {
    return (
-      <div className="w-full h-20 flex fixed px-[60px]">
+      <div className="w-full h-20 flex fixed px-[30px] bg-zinc-100/90  sm:bg-transparent">
          <div className="flex justify-start items-center ">
             <div className="">Logo aqui</div>
          </div>
-         <div className="w-full h-full flex items-center justify-end text-xl ">
-            <div className="flex justify-end mr-5 cursor-pointer">Tickets</div>
-            <div className="flex justify-end cursor-pointer">Contact us</div>
+         <div className="w-full h-full flex items-center justify-end text-xl">
+            <div className="flex flex-row justify-end mr-7 cursor-pointer gap-2">
+               <div className="hidden sm:block">Boletos</div>
+               <div>
+                  <BsTicketPerforated className="w-9 h-9 pb-1"/>
+               </div>
+            </div>
+            <div className="flex flex-row justify-end cursor-pointer gap-2">
+               <div className="hidden sm:block">Contacto</div>
+               <div>
+                  <MdOutlineEmail className="w-9 h-9 pb-1"/>
+               </div>
+            </div>
          </div>
       </div>
    )
