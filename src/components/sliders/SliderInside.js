7921789dd120fc2543168e6cc1ed967a65d40d30
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-03-13 22:07:34 -0500

    JE imgenes en el carrusel

--- src/components/sliders/SliderInside.js
+++ src/components/sliders/SliderInside.js
@@ -2,72 +2,58 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
+
+import img1 from "../../../public/image/car interior/int1.jpg"
+import img2 from "../../../public/image/car interior/int2.jpg"
+import img3 from "../../../public/image/car interior/int3.jpg"
+import img4 from "../../../public/image/car interior/int4.jpg"
+import img5 from "../../../public/image/car interior/int5.jpg"
+import img6 from "../../../public/image/car interior/int6.jpg"
+import img7 from "../../../public/image/car interior/int7.jpg"
+import img8 from "../../../public/image/car interior/int8.jpg"
+import img9 from "../../../public/image/car interior/int9.jpg"
+import img10 from "../../../public/image/car interior/int10.jpg"
+import img11 from "../../../public/image/car interior/int11.jpg"
+import img12 from "../../../public/image/car interior/int12.jpg"
+import img13 from "../../../public/image/car interior/int13.jpg"
+import img14 from "../../../public/image/car interior/int14.jpg"
+import img15 from "../../../public/image/car interior/int15.jpg"
 
 const SliderInside = () => {
 
+  const renderImages = () => {
+    const images = [
+      "",
+      "",
+      ""
+    ]
+
+  }
+
   var settings = {
-    dots: true,
     infinite: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    infinite: true,
+    arrows:false,
   };
 
 
   return (
     <div className="slider-container  ">
-        <div  className=" w-[400px] h-72">
-          <Slider  {...settings}>
-            <div className="w-[400px] h-[250px] bg-blue-500 flex justify-center items-center">
-              <div>1</div>
+        <div  className="  w-[300px] sm:w-[400px] lg:w-[500px] ">
+          <Slider {...settings} >
+            <div className="">
+              <Image src={img2} alt=""/>
             </div>
-            <div className="md:slider_img w-64 h-10 md:bg-yellow-500 bg-red-300 flex justify-center items-center">
-              <div>2</div>
+            <div>
+              <Image src={img3} alt=""/>
             </div>
-            <div className="w-80 h-64 bg-blue-500 flex justify-center items-center">
-              <div>3</div>
-            </div>
-            <div className="w-80 h-64 bg-red-300 flex justify-center items-center">
-              <div>4</div>
-            </div>
-            <div className="w-80 h-64 bg-blue-500 flex justify-center items-center">
-              <div>5</div>
-            </div>
-            <div className="w-80 h-64 bg-red-300 flex justify-center items-center">
-              <div>6</div>
-            </div>
-            <div className="w-80 h-64 bg-blue-500 flex justify-center items-center">
-              <div>7</div>
-            </div>
-            <div className="w-80 h-64 bg-red-300 flex justify-center items-center">
-              <div>8</div>
+            <div>
+              <Image src={img4} alt=""/>
             </div>
           </Slider>
         </div>
