4485a7c22101d258861e5092dac328d23d75c917
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-03-19 20:48:49 -0500

    JE cambio de fondo

--- src/components/pay/Payment.js
+++ src/components/pay/Payment.js
@@ -1,6 +1,6 @@
 const Payment = () => {
    return ( 
-      <div className="w-full h-screen flex justify-center pt-[100px] ">
+      <div className="w-full h-screen flex justify-center pt-[100px] bg_back">
          <div className="w-96  h-[500px] bg-zinc-700">
 
          </div>
